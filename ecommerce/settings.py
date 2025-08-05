fbc3d70479e0c742b3fc3fbb451936d761343672
prasath-m21 <mprasath0907@gmail.com>
2025-08-05 22:37:53 +0530

    Update ALLOWED_HOSTS

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','.onrender.com', 'django-ecommerce.onrender.com',]
 
 # Application definition
 INSTALLED_APPS = [
